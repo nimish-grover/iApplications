6ad189a8245aa5e8b7c3ea15c8f5bebf50a24162
Nimish grover <grovernimish2003@gmail.com>
2024-07-24 16:27:09 +0530

    zip added

--- iSaksham/app/__init__.py
+++ iSaksham/app/__init__.py
@@ -42,7 +42,7 @@ def create_app():
     # app.config["OPENAPI_URL_PREFIX"] = "/"
     # app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     # app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('ISAKSHAM_DATABASE_URL')  # Replace with your actual database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xgmkxzsp:CsB1HfTbEfktRnrL-0hcByzS7iyh1qkL@rain.db.elephantsql.com/xgmkxzsp'  # Replace with your actual database URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['PROPAGATE_EXCEPTIONS'] = True
 
