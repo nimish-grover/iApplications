193d1b035bcefd8f423d0dbb11916ccfeefaa38e
Nimish Grover <grovernimish2003@gmail.com>
2025-06-11 01:38:59 +0530

    wsl fix

--- iSaksham/app/__init__.py
+++ iSaksham/app/__init__.py
@@ -48,7 +48,7 @@ def create_app():
     # app.config["OPENAPI_URL_PREFIX"] = "/"
     # app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     # app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xgmkxzsp:CsB1HfTbEfktRnrL-0hcByzS7iyh1qkL@rain.db.elephantsql.com/xgmkxzsp'  # Replace with your actual database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://db_master:w24JyTn0SIEHfS@144.24.103.183:5432/isaksham_db'  # Replace with your actual database URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['PROPAGATE_EXCEPTIONS'] = True
     mail = Mail(app)
